cfb003ef436fa450e65db95553ba4255458d53ee
Jeff DeWall <sr.jilarious@fastmail.net>
2023-10-08 10:54:44 -0600

    Initial commit, pulling in zig-gamedev as submodule, creating basic SDL win.

---
+++ build.zig
@@ -0,0 +1,95 @@
+const std = @import("std");
+
+const zsdl = @import("libs/zig-gamedev/libs/zsdl/build.zig");
+
+// Although this function looks imperative, note that its job is to
+// declaratively construct a build graph that will be executed by an external
+// runner.
+pub fn build(b: *std.Build) void {
+    // Standard target options allows the person running `zig build` to choose
+    // what target to build for. Here we do not override the defaults, which
+    // means any target is allowed, and the default is native. Other options
+    // for restricting supported target set are available.
+    const target = b.standardTargetOptions(.{});
+
+    // Standard optimization options allow the person running `zig build` to select
+    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
+    // set a preferred release mode, allowing the user to decide how to optimize.
+    const optimize = b.standardOptimizeOption(.{});
+
+    const exe = b.addExecutable(.{
+        .name = "pixzig",
+        // In this case the main source file is merely a path, however, in more
+        // complicated build scripts, this could be a generated file.
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    // Build it
+    const zsdl_pkg = zsdl.package(b, target, optimize, .{});
+
+    // Link with your app
+    zsdl_pkg.link(exe);
+
+    // ** Wait for package management to be a bit more mature.
+    // // using duck as a dependency
+    // const zig_gamedev = b.dependency("zig_gamedev", .{
+    //     .target = target,
+    //     .optimize = optimize,
+    // });
+    // // duck has exported itself as duck
+    // // now you are re-exporting duck
+    // // as a module in your project with the name duck
+    // exe.addModule("zig_gamedev", zig_gamedev.module("zig_gamedev"));
+    //
+    // // you need to link to the output of the build process
+    // // that was done by the duck package
+    // // in this case, duck is outputting a library
+    // // to which your project need to link as well
+    // exe.linkLibrary(zig_gamedev.artifact("zig_gamedev"));
+
+    // This declares intent for the executable to be installed into the
+    // standard location when the user invokes the "install" step (the default
+    // step when running `zig build`).
+    b.installArtifact(exe);
+
+    // This *creates* a Run step in the build graph, to be executed when another
+    // step is evaluated that depends on it. The next line below will establish
+    // such a dependency.
+    const run_cmd = b.addRunArtifact(exe);
+
+    // By making the run step depend on the install step, it will be run from the
+    // installation directory rather than directly from within the cache directory.
+    // This is not necessary, however, if the application depends on other installed
+    // files, this ensures they will be present and in the expected location.
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    // This allows the user to pass arguments to the application in the build
+    // command itself, like this: `zig build run -- arg1 arg2 etc`
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    // This creates a build step. It will be visible in the `zig build --help` menu,
+    // and can be selected like this: `zig build run`
+    // This will evaluate the `run` step rather than the default, which is "install".
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+
+    // Creates a step for unit testing. This only builds the test executable
+    // but does not run it.
+    const unit_tests = b.addTest(.{
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const run_unit_tests = b.addRunArtifact(unit_tests);
+
+    // Similar to creating the run step earlier, this exposes a `test` step to
+    // the `zig build --help` menu, providing a way for the user to request
+    // running the unit tests.
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_unit_tests.step);
+}
---
+++ src/main.zig
@@ -0,0 +1,51 @@
+const std = @import("std");
+const sdl = @import("zsdl");
+
+pub fn main() !void {
+    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
+    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+
+    // stdout is for the actual output of your application, for example if you
+    // are implementing gzip, then only the compressed bytes should be sent to
+    // stdout, not any debugging messages.
+    const stdout_file = std.io.getStdOut().writer();
+    var bw = std.io.bufferedWriter(stdout_file);
+    const stdout = bw.writer();
+
+    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+
+    try bw.flush(); // don't forget to flush!
+
+    try sdl.init(.{ .audio = true, .video = true });
+    defer sdl.quit();
+
+    const window = try sdl.Window.create(
+        "zig-gamedev-window",
+        sdl.Window.pos_undefined,
+        sdl.Window.pos_undefined,
+        600,
+        600,
+        .{ .opengl = true, .allow_highdpi = true },
+    );
+    defer window.destroy();
+
+    main_loop: while (true) {
+        var event: sdl.Event = undefined;
+        while (sdl.pollEvent(&event)) {
+            if (event.type == .quit) {
+                break :main_loop;
+            } else if (event.type == .keydown) {
+                if (event.key.keysym.sym == .escape) break :main_loop;
+            }
+        }
+        // gl.clearBufferfv(gl.COLOR, 0, &[_]f32{ 0.2, 0.4, 0.8, 1.0 });
+        // sdl.gl.swapWindow(window);
+    }
+}
+
+test "simple test" {
+    var list = std.ArrayList(i32).init(std.testing.allocator);
+    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
+    try list.append(42);
+    try std.testing.expectEqual(@as(i32, 42), list.pop());
+}
