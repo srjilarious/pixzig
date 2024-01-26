48544cd5f9f16f2c2a5ad70cd0dfe5137c640ab3
Jeff DeWall <sr.jilarious@fastmail.net>
2024-01-26 06:42:58 -0700

    Refactored text rendering to use a struct to capture all the needed bits.

--- examples/text_rendering.zig
+++ examples/text_rendering.zig
@@ -30,51 +30,41 @@ pub const TextPixelShader: pixzig.shaders.ShaderCode =
     \\ }
 ;
 pub const Character = struct {
-    texId: c_uint,
     size: Vec2I,
     bearing: Vec2I,
     advance: u32
 };
 
 pub const TextRenderer = struct {
-    characters: std.AutoHashMap(u8, Character),
-};
-
-pub const MyApp = struct {
-    fps: FpsCounter,
-    alloc: std.mem.Allocator,
-    projMat: zmath.Mat,
-    spriteBatch: pixzig.renderer.SpriteBatchQueue,
+    characters: std.AutoHashMap(u32, Character),
     tex: pixzig.Texture,
+    spriteBatch: pixzig.renderer.SpriteBatchQueue,
+
     texShader: pixzig.shaders.Shader,
+    alloc: std.mem.Allocator,
 
-    pub fn init(eng: *PixzigEngine, alloc: std.mem.Allocator) !MyApp {
-        _ = eng;
-
-        // Orthographic projection matrix
-        const projMat = math.orthographicOffCenterLhGl(0, 800, 0, 600, -0.1, 1000);
-
+    pub fn init(fontFace: [:0]const u8, alloc: std.mem.Allocator) !TextRenderer {
+        const chars = std.AutoHashMap(u32, Character).init(alloc);
         var texShader = try pixzig.shaders.Shader.init(
             &pixzig.shaders.TexVertexShader,
             &TextPixelShader
         );
 
-        // const tex = try eng.textures.loadTexture("tiles", "assets/mario_grassish2.png");
-       
         const spriteBatch = try pixzig.renderer.SpriteBatchQueue.init(alloc, &texShader);
-        
 
         // Test font loading.
         const lib = try freetype.Library.init();
         defer lib.deinit();
 
-        const face = try lib.createFace("assets/Roboto-Medium.ttf", 0);
+        const face = try lib.createFace(fontFace, 0);
         try face.setCharSize(60 * 48, 0, 50, 0);
         // Generate a buffer for multiple glyphs
         const GlyphBufferWidth = 512;
         const GlyphBufferHeight = 512;
         var glyphBuffer = try alloc.alloc(u8, GlyphBufferWidth*GlyphBufferHeight);
         defer alloc.free(glyphBuffer);
+
+        
 
         var currY: usize = 0;
         var currLineMaxY: usize = 0;
@@ -112,7 +102,6 @@ pub const MyApp = struct {
 
         // generate texture
         var charTex: c_uint = undefined;
-        //gl.pixelStorei(gl.UNPACK_ALIGNMENT, 1); 
         gl.genTextures(1, &charTex);
         gl.bindTexture(gl.TEXTURE_2D, charTex);
         gl.texImage2D(
@@ -126,6 +115,38 @@ pub const MyApp = struct {
             gl.UNSIGNED_BYTE,
             @ptrCast(glyphBuffer)
         );
+
+        return .{ 
+            .alloc = alloc,
+            .characters = chars,
+            .spriteBatch = spriteBatch,
+            .tex = .{ 
+                .texture = charTex, 
+                .size = .{ 
+                    .x = @intCast(GlyphBufferWidth), 
+                    .y = @intCast(GlyphBufferHeight)
+                },
+                .name = null
+            },
+            .texShader = texShader,
+        };
+
+    }
+};
+
+pub const MyApp = struct {
+    fps: FpsCounter,
+    alloc: std.mem.Allocator,
+    projMat: zmath.Mat,
+    textRenderer: TextRenderer,
+
+    pub fn init(eng: *PixzigEngine, alloc: std.mem.Allocator) !MyApp {
+        _ = eng;
+
+        // Orthographic projection matrix
+        const projMat = math.orthographicOffCenterLhGl(0, 800, 0, 600, -0.1, 1000);
+
+        const textRenderer = try TextRenderer.init("assets/Roboto-Medium.ttf", alloc);
         
         // set texture options
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
@@ -138,16 +159,7 @@ pub const MyApp = struct {
             .fps = FpsCounter.init(),
             .alloc = alloc,
             .projMat = projMat,
-            .spriteBatch = spriteBatch,
-            .tex = .{ 
-                .texture = charTex, 
-                .size = .{ 
-                    .x = @intCast(GlyphBufferWidth), 
-                    .y = @intCast(GlyphBufferHeight)
-                },
-                .name = null
-            },
-            .texShader = texShader,
+            .textRenderer = textRenderer
         };
     }
 
@@ -169,10 +181,10 @@ pub const MyApp = struct {
         gl.clearBufferfv(gl.COLOR, 0, &[_]f32{ 0.0, 0.0, 0.2, 1.0 });
         self.fps.renderTick();
 
-        self.spriteBatch.begin(self.projMat);
-        self.spriteBatch.drawSprite(&self.tex, 
+        self.textRenderer.spriteBatch.begin(self.projMat);
+        self.textRenderer.spriteBatch.drawSprite(&self.textRenderer.tex, 
             RectF.fromPosSize(32, 32, 512, 512), .{ .l=0, .t=0, .r=1, .b=1});
-        self.spriteBatch.end();
+        self.textRenderer.spriteBatch.end();
     }
 };
 
