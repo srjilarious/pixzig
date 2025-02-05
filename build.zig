a56db581c679c452664adf0140f60a1838c2dd69
Jeff DeWall <sr.jilarious@fastmail.net>
2025-02-04 21:00:05 -0700

    Fixed web builds to handle multiple different examples, with each specifying which assets they need.

--- build.zig
+++ build.zig
@@ -1,16 +1,10 @@
-// zig fmt: off
-
 const std = @import("std");
 const builtin = std.builtin;
 
 const assets_dir = "assets/";
 
-fn addArchIncludes(b: *std.Build, 
-    target: std.Build.ResolvedTarget, 
-    optimize: std.builtin.OptimizeMode, 
-    dep: *std.Build.Step.Compile) !void
-{
-    _  = optimize;
+fn addArchIncludes(b: *std.Build, target: std.Build.ResolvedTarget, optimize: std.builtin.OptimizeMode, dep: *std.Build.Step.Compile) !void {
+    _ = optimize;
     switch (target.result.os.tag) {
         .emscripten => {
             if (b.sysroot == null) {
@@ -21,30 +15,35 @@ fn addArchIncludes(b: *std.Build,
             const cache_include = std.fs.path.join(b.allocator, &.{ b.sysroot.?, "include" }) catch @panic("Out of memory");
             defer b.allocator.free(cache_include);
 
-            var dir = std.fs.openDirAbsolute(cache_include, std.fs.Dir.OpenDirOptions{ .access_sub_paths = true, .no_follow = true }) catch @panic("No emscripten cache. Generate it!");
+            var dir = std.fs.openDirAbsolute(cache_include, std.fs.Dir.OpenDirOptions{ .access_sub_paths = true, .no_follow = true }) catch {
+                @panic("No emscripten cache. Generate it!");
+            };
+
             dir.close();
             dep.addIncludePath(.{ .cwd_relative = cache_include });
         },
-        else => {}
+        else => {},
     }
 }
 
-pub fn example(b: *std.Build, 
-    target: std.Build.ResolvedTarget, 
-    optimize: std.builtin.OptimizeMode, 
-    name: []const u8, 
-    root_src_path: []const u8) *std.Build.Step.Compile
-{
+// Each example links in multiple different libraries, so this function sets that up for each one.
+pub fn example(
+    b: *std.Build,
+    target: std.Build.ResolvedTarget,
+    optimize: std.builtin.OptimizeMode,
+    comptime name: []const u8,
+    comptime root_src_path: []const u8,
+    comptime assets: []const []const u8,
+) *std.Build.Step.Compile {
     const exe = blk: {
-        if(target.result.os.tag == .emscripten) {
+        if (target.result.os.tag == .emscripten) {
             break :blk b.addStaticLibrary(.{
                 .name = name,
                 .root_source_file = b.path(root_src_path),
                 .target = target,
                 .optimize = optimize,
             });
-        }
-        else {
+        } else {
             break :blk b.addExecutable(.{
                 .name = name,
                 .root_source_file = b.path(root_src_path),
@@ -69,18 +68,17 @@ pub fn example(b: *std.Build,
     try addArchIncludes(b, target, optimize, stbi_dep);
 
     const zmath = b.dependency("zmath", .{ .target = target });
-    
+
     const zgui = blk: {
-        if(target.result.os.tag == .emscripten) {
-            break :blk b.dependency("zgui", .{ 
+        if (target.result.os.tag == .emscripten) {
+            break :blk b.dependency("zgui", .{
                 .target = target,
                 .backend = .glfw, // emscripten
                 // .shared = false,
                 // .with_implot = true,
             });
-        }
-        else {
-            break :blk b.dependency("zgui", .{ 
+        } else {
+            break :blk b.dependency("zgui", .{
                 .target = target,
                 .backend = .glfw_opengl3,
                 // .shared = false,
@@ -92,13 +90,9 @@ pub fn example(b: *std.Build,
     const gui_dep = zgui.artifact("imgui");
     try addArchIncludes(b, target, optimize, gui_dep);
 
-    const ziglua = b.dependency("ziglua", .{
-        .target = target,
-        .optimize = optimize,
-        .lang = .lua53
-    });
-    ziglua.module("ziglua").addIncludePath(.{.cwd_relative = "/home/jeffdw/.cache/emscripten/sysroot/include"});
-    ziglua.module("ziglua-c").addIncludePath(.{.cwd_relative = "/home/jeffdw/.cache/emscripten/sysroot/include"});
+    const ziglua = b.dependency("ziglua", .{ .target = target, .optimize = optimize, .lang = .lua53 });
+    ziglua.module("ziglua").addIncludePath(.{ .cwd_relative = "/home/jeffdw/.cache/emscripten/sysroot/include" });
+    ziglua.module("ziglua-c").addIncludePath(.{ .cwd_relative = "/home/jeffdw/.cache/emscripten/sysroot/include" });
 
     const lua_dep = ziglua.artifact("lua");
     try addArchIncludes(b, target, optimize, lua_dep);
@@ -112,13 +106,13 @@ pub fn example(b: *std.Build,
         .root_source_file = b.path("src/pixzig/pixzig.zig"),
     });
 
-    if(target.result.os.tag != .emscripten) {
+    if (target.result.os.tag != .emscripten) {
         const glfw_dep = zglfw.artifact("glfw");
 
         // Use mach-freetype
-        // const freetype = b.dependency("freetype", .{ 
-        //         .target = target, 
-        //         .optimize = optimize, 
+        // const freetype = b.dependency("freetype", .{
+        //         .target = target,
+        //         .optimize = optimize,
         //         .enable_brotli=false
         //     });
         // const freetype_dep = freetype.artifact("freetype");
@@ -129,12 +123,11 @@ pub fn example(b: *std.Build,
         //     .optimize = optimize,
         //     .enable_brotli = false,
         // });
-    
+
         // Link with your app
-    
-        
+
         exe.linkLibrary(glfw_dep);
-        
+
         exe.linkLibrary(gui_dep);
         exe.linkLibrary(stbi_dep);
         // exe.linkLibrary(freetype_dep);
@@ -144,41 +137,39 @@ pub fn example(b: *std.Build,
         // pixeng.addImport("freetype", freetype_mod);
     }
 
-
     // Use GLFW for GL context, windowing, input, etc.
     const zglfw_mod = zglfw.module("root");
     pixeng.addImport("zglfw", zglfw_mod);
     exe.root_module.addImport("zglfw", zglfw_mod);
-    
+
     // OpenGL
     const gl_mod = zopengl.module("root");
     exe.root_module.addImport("zopengl", gl_mod);
     pixeng.addImport("zopengl", gl_mod);
-    
+
     // GUI support
     const zgui_mod = zgui.module("root");
-    exe.root_module.addImport("zgui",zgui_mod );
+    exe.root_module.addImport("zgui", zgui_mod);
     pixeng.addImport("zgui", zgui_mod);
     // std.debug.print("zgui mod has {} include dirs.\n", .{zgui_mod.include_dirs.items.len});
-    
+
     // STBI for image loading.
     const zstbi_mod = zstbi.module("root");
     pixeng.addImport("zstbi", zstbi_mod);
-    
+
     // Math library
     const math_mod = zmath.module("root");
     pixeng.addImport("zmath", math_mod);
     exe.root_module.addImport("zmath", math_mod);
-    
+
     // ECS library.
     const zflecs_mod = zflecs.module("root");
     exe.root_module.addImport("zflecs", zflecs_mod);
     pixeng.addImport("zflecs", zflecs_mod);
 
     // XML for tilemap loading.
-    const xml = b.addModule("xml", .{ .root_source_file = b.path("libs/xml.zig")});
+    const xml = b.addModule("xml", .{ .root_source_file = b.path("libs/xml.zig") });
     pixeng.addImport("xml", xml);
-    
 
     // add the ziglua module and lua artifact
     const ziglua_mod = ziglua.module("ziglua");
@@ -188,7 +179,6 @@ pub fn example(b: *std.Build,
 
     exe.root_module.addImport("pixzig", pixeng);
 
-        
     const install_content_step = b.addInstallDirectory(.{
         .source_dir = b.path(assets_dir),
         .install_dir = .{ .custom = "" },
@@ -224,7 +214,6 @@ pub fn example(b: *std.Build,
     const run_step = b.step(name, "Run example");
     run_step.dependOn(&run_cmd.step);
 
-
     // const emcc_exe = switch (builtin.os.tag) { // TODO bundle emcc as a build dependency
     //     .windows => "emcc.bat",
     //     else => "emcc",
@@ -232,11 +221,26 @@ pub fn example(b: *std.Build,
 
     switch (target.result.os.tag) {
         .emscripten => {
+            const path = std.fmt.allocPrint(b.allocator, "{s}/web/{s}", .{ b.install_prefix, name }) catch unreachable;
+            defer b.allocator.free(path);
+
+            std.debug.print("Installing to: {s}\n", .{path});
+
+            const index_path = std.fmt.allocPrint(b.allocator, "{s}/index.html", .{path}) catch unreachable;
+            defer b.allocator.free(index_path);
+
+            const mkdir_command = b.addSystemCommand(&[_][]const u8{"mkdir"});
+            mkdir_command.addArg(path);
+
             const emcc_exe_path = "/usr/lib/emscripten/em++";
             const emcc_command = b.addSystemCommand(&[_][]const u8{emcc_exe_path});
+
+            // We need our web subdirectory to exist for em++ to be able to run.
+            emcc_command.step.dependOn(&mkdir_command.step);
+
             emcc_command.addArgs(&[_][]const u8{
                 "-o",
-                "zig-out/web/index.html",
+                index_path,
                 "-sFULL-ES3=1",
                 "-sUSE_GLFW=3",
                 "-O3",
@@ -265,16 +269,21 @@ pub fn example(b: *std.Build,
                 "-sSTACK_SIZE=2mb",
 
                 // Test embedding some graphics
-                "--preload-file", "assets/mario_grassish2.png",
-                "--preload-file", "assets/digcraft_sprites.png",
-                "--preload-file", "assets/digcraft_sprites.json",
-                "--preload-file", "assets/test.lua",
-                "--preload-file", "assets/digconf.lua",
-                "--preload-file", "assets/level1a.tmx",
+                // "--preload-file", "assets/mario_grassish2.png",
+                // "--preload-file", "assets/digcraft_sprites.png",
+                // "--preload-file", "assets/digcraft_sprites.json",
+                // "--preload-file", "assets/test.lua",
+                // "--preload-file", "assets/digconf.lua",
+                // "--preload-file", "assets/level1a.tmx",
 
                 "--shell-file",
                 b.path("src/shell.html").getPath(b),
             });
+
+            // Add all of the specified assets.
+            for (assets) |asset| {
+                emcc_command.addArgs(&[_][]const u8{ "--preload-file", asset });
+            }
 
             const link_items: []const *std.Build.Step.Compile = &.{
                 stbi_dep,
@@ -292,15 +301,13 @@ pub fn example(b: *std.Build,
             const install = emcc_command;
             b.default_step.dependOn(&install.step);
         },
-        else => {}
+        else => {},
     }
 
     return exe;
 }
 
-// Although this function looks imperative, note that its job is to
-// declaratively construct a build graph that will be executed by an external
-// runner.
+// Construct the build graph
 pub fn build(b: *std.Build) void {
     // Standard target options allows the person running `zig build` to choose
     // what target to build for. Here we do not override the defaults, which
@@ -313,34 +320,73 @@ pub fn build(b: *std.Build) void {
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
+    // Digcraft.
+    _ = example(
+        b,
+        target,
+        optimize,
+        "digcraft",
+        "games/digcraft/digcraft.zig",
+        &.{
+            "assets/mario_grassish2.png",
+            "assets/digcraft_sprites.png",
+            "assets/digcraft_sprites.json",
+            "assets/digconf.lua",
+            "assets/level1a.tmx",
+        },
+    );
 
-    _ = example(b, target, optimize, "digcraft", "games/digcraft/digcraft.zig");
-    if(target.result.os.tag != .emscripten) {
-        _ = example(b, target, optimize, "actor_test", "examples/actor_test.zig");
-        _ = example(b, target, optimize, "collision_test", "examples/collision_test.zig");
-        _ = example(b, target, optimize, "create_texture", "examples/create_texture.zig");
-        _ = example(b, target, optimize, "flecs_test", "examples/flecs_test.zig");
-        _ = example(b, target, optimize, "gameloop_test", "examples/gameloop_test.zig");
-        _ = example(b, target, optimize, "game_state_test", "examples/game_state_test.zig");
-        _ = example(b, target, optimize, "glfw_sprites", "examples/glfw_sprites.zig");
-        _ = example(b, target, optimize, "grid_render", "examples/grid_render.zig");
-        _ = example(b, target, optimize, "lua_test", "examples/lua_test.zig");
-        _ = example(b, target, optimize, "mouse_test", "examples/mouse_test.zig");
-        _ = example(b, target, optimize, "tile_load_test", "examples/tile_load_test.zig");
+    // Tile map test.
+    _ = example(
+        b,
+        target,
+        optimize,
+        "tile_load_test",
+        "examples/tile_load_test.zig",
+        &.{
+            "assets/mario_grassish2.png",
+            "assets/level1a.tmx",
+        },
+    );
+
+    _ = example(b, target, optimize, "natetris", "games/natetris/natetris.zig", &.{});
+
+    if (target.result.os.tag != .emscripten) {
+        _ = example(b, target, optimize, "actor_test", "examples/actor_test.zig", &.{
+            "assets/mario_grassish2.png",
+        });
+        _ = example(b, target, optimize, "collision_test", "examples/collision_test.zig", &.{
+            "assets/mario_grassish2.png",
+            "assets/level1a.tmx",
+        });
+        _ = example(b, target, optimize, "create_texture", "examples/create_texture.zig", &.{});
+        _ = example(b, target, optimize, "flecs_test", "examples/flecs_test.zig", &.{
+            "assets/mario_grassish2.png",
+        });
+        _ = example(b, target, optimize, "gameloop_test", "examples/gameloop_test.zig", &.{});
+        _ = example(b, target, optimize, "game_state_test", "examples/game_state_test.zig", &.{});
+        _ = example(b, target, optimize, "glfw_sprites", "examples/glfw_sprites.zig", &.{
+            "assets/mario_grassish2.png",
+        });
+        _ = example(b, target, optimize, "grid_render", "examples/grid_render.zig", &.{});
+        _ = example(b, target, optimize, "lua_test", "examples/lua_test.zig", &.{
+            "assets/test.lua",
+        });
+        _ = example(b, target, optimize, "mouse_test", "examples/mouse_test.zig", &.{
+            "assets/mario_grassish2.png",
+        });
         // // _ = example(b, target, optimize, "a_star_path", "examples/a_star_path.zig");
-        _ = example(b, target, optimize, "console_test", "examples/console_test.zig");
-    //     _ = example(b, target, optimize, "text_rendering", "examples/text_rendering.zig");
+        _ = example(b, target, optimize, "console_test", "examples/console_test.zig", &.{});
+        //     _ = example(b, target, optimize, "text_rendering", "examples/text_rendering.zig");
 
-        _ = example(b, target, optimize, "natetris", "games/natetris/natetris.zig");
-
-        const spack = example(b, target, optimize, "spack", "tools/spack/spack.zig");
+        const spack = example(b, target, optimize, "spack", "tools/spack/spack.zig", &.{});
         const zargs = b.dependency("zargunaught", .{});
         spack.root_module.addImport("zargunaught", zargs.module("zargunaught"));
 
         const zstbi = b.dependency("zstbi", .{ .target = target });
         spack.root_module.addImport("zstbi", zstbi.module("root"));
 
-        const tests = example(b, target, optimize, "tests", "tests/main.zig");
+        const tests = example(b, target, optimize, "tests", "tests/main.zig", &.{});
         const testzMod = b.dependency("testz", .{});
         tests.root_module.addImport("testz", testzMod.module("testz"));
     }
