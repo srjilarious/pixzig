cea19c3932c8f98cf220c97d8b946da02fe17d0f
Jeff DeWall <sr.jilarious@fastmail.net>
2025-02-25 20:47:10 -0700

    Added in tile rendering to a_star path, WIP towards drawing path

--- examples/a_star_path.zig
+++ examples/a_star_path.zig
@@ -26,10 +26,22 @@ const CharToColor = pixzig.textures.CharToColor;
 const Color8 = pixzig.Color8;
 const Texture = pixzig.textures.Texture;
 
+const Path = pixzig.a_star.Path;
+const AStarPathFinder = pixzig.a_star.AStarPathFinder;
+const TileLayer = pixzig.tile.TileLayer;
+const TileSet = pixzig.tile.TileSet;
+const Tile = pixzig.tile.Tile;
+const TileMapRenderer = pixzig.tile.TileMapRenderer;
+
 const NumTilesHorz = 4;
 const NumTilesVert = 4;
 const TileWidth = 8;
 const TileHeight = 8;
+
+const BasicTileMapPathChecker = pixzig.a_star.BasicTileMapPathChecker;
+
+const MapWidth = 24;
+const MapHeight = 18;
 
 // Sets up the panic handler and log handler depending on the OS target.
 pub const panic = pixzig.system.panic;
@@ -42,16 +54,23 @@ pub const App = struct {
     projMat: zmath.Mat,
     fps: FpsCounter,
     grid: GridRenderer,
+    path: Path,
+    layer: TileLayer,
+    pathLayer: TileLayer,
+    pathLayerRenderer: TileMapRenderer,
+    tileSet: TileSet,
+    checker: BasicTileMapPathChecker,
+    pathFinder: AStarPathFinder(BasicTileMapPathChecker),
     tex: *Texture,
 
     pub fn init(alloc: std.mem.Allocator, eng: *AppRunner.Engine) !*App {
         const app = try alloc.create(App);
 
         // Orthographic projection matrix
-        const projMat = math.orthographicOffCenterLhGl(0, 800, 0, 600, -0.1, 1000);
+        const projMat = math.orthographicOffCenterLhGl(0, 200, 0, 150, -0.1, 1000);
 
         const shader = try Shader.init(&shaders.ColorVertexShader, &shaders.ColorPixelShader);
-        const grid = try GridRenderer.init(alloc, shader, .{ .x = 20, .y = 12 }, .{ .x = 32, .y = 32 }, 1, Color{ .r = 1.0, .g = 1.0, .b = 1.0, .a = 1.0 });
+        const grid = try GridRenderer.init(alloc, shader, .{ .x = MapWidth, .y = MapHeight }, .{ .x = TileWidth, .y = TileHeight }, 1, Color{ .r = 1.0, .g = 1.0, .b = 1.0, .a = 1.0 });
 
         // Create a texture for the path tiles.
         const colorMap = &[_]CharToColor{
@@ -63,6 +82,16 @@ pub const App = struct {
             .{ .char = ' ', .color = Color8.from(0, 0, 0, 0) },
         };
 
+        const emptyChars =
+            \\        
+            \\        
+            \\        
+            \\        
+            \\        
+            \\        
+            \\        
+            \\        
+        ;
         const lockedChars =
             \\=------=
             \\-######-
@@ -145,13 +174,14 @@ pub const App = struct {
 
         const bufferSize = Vec2U{ .x = NumTilesHorz * TileWidth, .y = NumTilesVert * TileHeight };
 
-        pixzig.textures.drawBufferFromChars(textureBuff, bufferSize, lockedChars, .{ .x = TileWidth, .y = TileHeight }, .{ .x = 0, .y = 0 }, colorMap);
-        pixzig.textures.drawBufferFromChars(textureBuff, bufferSize, horzChars, .{ .x = TileWidth, .y = TileHeight }, .{ .x = 8, .y = 0 }, colorMap);
-        pixzig.textures.drawBufferFromChars(textureBuff, bufferSize, vertChars, .{ .x = TileWidth, .y = TileHeight }, .{ .x = 16, .y = 0 }, colorMap);
-        pixzig.textures.drawBufferFromChars(textureBuff, bufferSize, topLeftChars, .{ .x = TileWidth, .y = TileHeight }, .{ .x = 24, .y = 0 }, colorMap);
-        pixzig.textures.drawBufferFromChars(textureBuff, bufferSize, topRightChars, .{ .x = TileWidth, .y = TileHeight }, .{ .x = 0, .y = 8 }, colorMap);
-        pixzig.textures.drawBufferFromChars(textureBuff, bufferSize, bottomLeftChars, .{ .x = TileWidth, .y = TileHeight }, .{ .x = 8, .y = 8 }, colorMap);
-        pixzig.textures.drawBufferFromChars(textureBuff, bufferSize, bottomRightChars, .{ .x = TileWidth, .y = TileHeight }, .{ .x = 16, .y = 8 }, colorMap);
+        pixzig.textures.drawBufferFromChars(textureBuff, bufferSize, emptyChars, .{ .x = TileWidth, .y = TileHeight }, .{ .x = 0, .y = 0 }, colorMap);
+        pixzig.textures.drawBufferFromChars(textureBuff, bufferSize, lockedChars, .{ .x = TileWidth, .y = TileHeight }, .{ .x = 8, .y = 0 }, colorMap);
+        pixzig.textures.drawBufferFromChars(textureBuff, bufferSize, horzChars, .{ .x = TileWidth, .y = TileHeight }, .{ .x = 16, .y = 0 }, colorMap);
+        pixzig.textures.drawBufferFromChars(textureBuff, bufferSize, vertChars, .{ .x = TileWidth, .y = TileHeight }, .{ .x = 24, .y = 0 }, colorMap);
+        pixzig.textures.drawBufferFromChars(textureBuff, bufferSize, topLeftChars, .{ .x = TileWidth, .y = TileHeight }, .{ .x = 0, .y = 8 }, colorMap);
+        pixzig.textures.drawBufferFromChars(textureBuff, bufferSize, topRightChars, .{ .x = TileWidth, .y = TileHeight }, .{ .x = 8, .y = 8 }, colorMap);
+        pixzig.textures.drawBufferFromChars(textureBuff, bufferSize, bottomLeftChars, .{ .x = TileWidth, .y = TileHeight }, .{ .x = 16, .y = 8 }, colorMap);
+        pixzig.textures.drawBufferFromChars(textureBuff, bufferSize, bottomRightChars, .{ .x = TileWidth, .y = TileHeight }, .{ .x = 24, .y = 8 }, colorMap);
         // const wallTex = try eng.textures.createTextureFromChars("wall", 8, 8, lockedChars, );
 
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT);
@@ -159,14 +189,46 @@ pub const App = struct {
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
         const tex = try eng.textures.loadTextureFromBuffer("a_star", bufferSize.x, bufferSize.y, textureBuff);
+
         app.* = .{
             .alloc = alloc,
             .projMat = projMat,
             .fps = FpsCounter.init(),
             .grid = grid,
             .tex = tex,
+            .path = Path.init(alloc),
+            .layer = undefined,
+            .pathLayer = undefined,
+            .pathLayerRenderer = undefined,
+            .tileSet = undefined,
+            .checker = undefined,
+            .pathFinder = undefined,
         };
 
+        app.tileSet = try TileSet.initEmpty(alloc, .{ .x = 8, .y = 8 }, .{ .x = 32, .y = 32 }, 8);
+        app.tileSet.tile(0).?.* = Tile{ .core = pixzig.tile.Clear, .properties = null, .alloc = alloc };
+        app.tileSet.tile(1).?.* = Tile{ .core = pixzig.tile.BlocksAll, .properties = null, .alloc = alloc };
+
+        app.layer = try TileLayer.initEmpty(alloc, .{ .x = MapWidth, .y = MapHeight }, .{ .x = TileWidth, .y = TileHeight });
+        app.layer.tileset = &app.tileSet;
+
+        app.layer.setTileData(1, 0, 1);
+        app.layer.setTileData(2, 2, 1);
+        app.layer.setTileData(3, 4, 1);
+        app.layer.setTileData(5, 5, 1);
+        app.layer.setTileData(5, 6, 1);
+        app.layer.setTileData(4, 6, 1);
+        app.layer.setTileData(7, 7, 1);
+
+        app.pathLayer = try TileLayer.initEmpty(alloc, .{ .x = MapWidth, .y = MapHeight }, .{ .x = TileWidth, .y = TileHeight });
+        app.pathLayer.tileset = &app.tileSet;
+
+        app.pathLayerRenderer = try tile.TileMapRenderer.init(alloc, eng.renderer.impl.texShader);
+        // TODO: Change to pathLayer and make wall layer renderer.
+        try app.pathLayerRenderer.recreateVertices(&app.tileSet, &app.layer);
+
+        app.checker = BasicTileMapPathChecker.init(&app.layer);
+        app.pathFinder = try AStarPathFinder(BasicTileMapPathChecker).init(app.checker, alloc, .{ .x = MapWidth, .y = MapHeight });
         return app;
     }
 
@@ -181,6 +243,13 @@ pub const App = struct {
         }
 
         eng.keyboard.update();
+        if (eng.keyboard.pressed(.space)) {
+            self.pathFinder.findPath(.{ .x = 0, .y = 0 }, .{ .x = 2, .y = 0 }, &self.path) catch {
+                std.log.err("Unable to calculate the path!", .{});
+            };
+            std.log.info("Path is {} nodes long", .{self.path.items.len});
+        }
+
         if (eng.keyboard.pressed(.escape)) {
             return false;
         }
@@ -193,9 +262,11 @@ pub const App = struct {
         eng.renderer.begin(self.projMat);
 
         eng.renderer.clear(0.0, 0.0, 0.2, 1.0);
-        eng.renderer.drawFullTexture(self.tex, .{ .x = 0, .y = 0 }, 8);
+        //eng.renderer.drawFullTexture(self.tex, .{ .x = 0, .y = 0 }, 8);
 
         eng.renderer.end();
+
+        try self.pathLayerRenderer.draw(self.tex, &self.layer, self.projMat);
 
         try self.grid.draw(self.projMat);
     }
