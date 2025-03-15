8bc5a7cde9ab2c663f8909ea9ea163977f24e12c
Jeff DeWall <sr.jilarious@fastmail.net>
2025-03-15 10:48:31 -0600

    Upgraded to zig 0.14.0

--- libs/zflecs/src/zflecs.zig
+++ libs/zflecs/src/zflecs.zig
@@ -513,7 +513,7 @@ pub const term_t = extern struct {
 pub const query_t = extern struct {
     hdr: header_t = .{},
 
-    terms: [FLECS_TERM_COUNT_MAX]term_t = .{.{}} ** FLECS_TERM_COUNT_MAX,
+    terms: [FLECS_TERM_COUNT_MAX]term_t = .{term_t{}} ** FLECS_TERM_COUNT_MAX,
     sizes: [FLECS_TERM_COUNT_MAX]size_t = .{0} ** FLECS_TERM_COUNT_MAX,
     ids: [FLECS_TERM_COUNT_MAX]id_t = .{0} ** FLECS_TERM_COUNT_MAX,
 
