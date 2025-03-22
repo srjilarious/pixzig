7373d11a5bd5565f6b709aa7e9a66d8c645a1de3
Jeff DeWall <sr.jilarious@fastmail.net>
2025-03-22 08:33:00 -0600

    Cleaned up key chord code to start actual implementation finally.

--- src/pixzig/input.zig
+++ src/pixzig/input.zig
@@ -1,4 +1,3 @@
-// zig fmt: off
 const std = @import("std");
 const glfw = @import("zglfw");
 const comp = @import("./comp.zig");
@@ -9,7 +8,7 @@ const Vec2F = common.Vec2F;
 const NumKeys = comp.numEnumFields(glfw.Key);
 
 fn getIndexForKey(key: glfw.Key) usize {
-   const enumTypeInfo = @typeInfo(glfw.Key).@"enum";
+    const enumTypeInfo = @typeInfo(glfw.Key).@"enum";
     comptime var keyIdx: usize = 0;
     inline for (enumTypeInfo.fields) |field| {
         const fieldKey = @field(glfw.Key, field.name);
@@ -25,9 +24,7 @@ pub const KeyboardState = struct {
 
     pub fn init() KeyboardState {
         const keys = std.StaticBitSet(NumKeys).initEmpty();
-        return .{ 
-            .keys = keys
-        };
+        return .{ .keys = keys };
     }
 
     pub fn down(self: *KeyboardState, keyIdx: usize) bool {
@@ -36,10 +33,9 @@ pub const KeyboardState = struct {
     }
 
     pub fn set(self: *KeyboardState, keyIdx: usize, val: bool) void {
-        if(val) {
+        if (val) {
             self.keys.set(keyIdx);
-        }
-        else {
+        } else {
             self.keys.unset(keyIdx);
         }
     }
@@ -53,16 +49,7 @@ pub const Keyboard = struct {
     allocator: std.mem.Allocator,
 
     pub fn init(win: *glfw.Window, alloc: std.mem.Allocator) Keyboard {
-        const res: Keyboard = .{
-            .currIdx = 0, 
-            .prevIdx = 1,
-            .keyBuffers = .{
-                KeyboardState.init(),
-                KeyboardState.init()
-            },
-            .window = win,
-            .allocator = alloc
-        };
+        const res: Keyboard = .{ .currIdx = 0, .prevIdx = 1, .keyBuffers = .{ KeyboardState.init(), KeyboardState.init() }, .window = win, .allocator = alloc };
 
         return res;
     }
@@ -82,7 +69,6 @@ pub const Keyboard = struct {
         var curr = self.currKeys();
         //const prev = self.prevKeys();
         // curr.keys.setRangeValue(.{.start=0, .end=NumKeys}, false);
-       
 
         // Update the current keys
         const enumTypeInfo = @typeInfo(glfw.Key).@"enum";
@@ -117,13 +103,12 @@ pub const Keyboard = struct {
     }
 };
 
-
 // ----------------------------------------------------------------------------
 // Mouse functionality
 const NumMouseButtons = comp.numEnumFields(glfw.MouseButton);
 
 fn getIndexForMouseButton(key: glfw.MouseButton) usize {
-   const enumTypeInfo = @typeInfo(glfw.MouseButton).@"enum";
+    const enumTypeInfo = @typeInfo(glfw.MouseButton).@"enum";
     comptime var keyIdx: usize = 0;
     inline for (enumTypeInfo.fields) |field| {
         const fieldKey = @field(glfw.MouseButton, field.name);
@@ -140,10 +125,7 @@ pub const MouseState = struct {
 
     pub fn init() MouseState {
         const buttons = std.StaticBitSet(NumMouseButtons).initEmpty();
-        return .{ 
-            .buttons = buttons,
-            .pos = .{ .x = 0, .y = 0 }
-        };
+        return .{ .buttons = buttons, .pos = .{ .x = 0, .y = 0 } };
     }
 
     pub fn down(self: *MouseState, keyIdx: usize) bool {
@@ -152,10 +134,9 @@ pub const MouseState = struct {
     }
 
     pub fn set(self: *MouseState, btnIdx: usize, val: bool) void {
-        if(val) {
+        if (val) {
             self.buttons.set(btnIdx);
-        }
-        else {
+        } else {
             self.buttons.unset(btnIdx);
         }
     }
@@ -173,16 +154,7 @@ pub const Mouse = struct {
     allocator: std.mem.Allocator,
 
     pub fn init(win: *glfw.Window, alloc: std.mem.Allocator) Mouse {
-        const res: Mouse = .{
-            .currIdx = 0, 
-            .prevIdx = 1,
-            .mouseBuffers = .{
-                MouseState.init(),
-                MouseState.init()
-            },
-            .window = win,
-            .allocator = alloc
-        };
+        const res: Mouse = .{ .currIdx = 0, .prevIdx = 1, .mouseBuffers = .{ MouseState.init(), MouseState.init() }, .window = win, .allocator = alloc };
 
         return res;
     }
@@ -195,7 +167,7 @@ pub const Mouse = struct {
         var state = self.curr();
         //const prev = self.prevKeys();
         // curr.keys.setRangeValue(.{.start=0, .end=NumKeys}, false);
-       
+
         // Update the current keys
         const enumTypeInfo = @typeInfo(glfw.MouseButton).@"enum";
         comptime var btnIdx = 0;
@@ -208,7 +180,6 @@ pub const Mouse = struct {
         state.setPos(self.window.getCursorPos());
     }
 
-
     fn curr(self: *Mouse) *MouseState {
         return &self.mouseBuffers[self.currIdx];
     }
@@ -216,7 +187,6 @@ pub const Mouse = struct {
     fn prev(self: *Mouse) *MouseState {
         return &self.mouseBuffers[self.prevIdx];
     }
-
 
     pub fn up(self: *Mouse, btn: glfw.MouseButton) bool {
         const btnIdx = getIndexForMouseButton(btn);
@@ -247,24 +217,11 @@ pub const Mouse = struct {
     }
 };
 
-
-pub const KeyModifier = enum(u8) {
-    none = 0x0,
-    ctrl = 0x1,
-    alt = 0x2,
-    ctrl_alt = 0x3,
-    shift = 0x4,
-    ctrl_shift = 0x5,
-    alt_shift = 0x6,
-    ctrl_alt_shift = 0x7,
-    super = 0x8,
-    ctrl_super = 0x9,
-    alt_super = 0xa,
-    ctrl_alt_super = 0xb,
-    shift_super = 0xc,
-    ctrl_shift_super = 0xd,
-    alt_shift_super = 0xe,
-    ctrl_alt_shift_super = 0xf
+pub const KeyModifier = struct {
+    ctrl: bool = false,
+    alt: bool = false,
+    shift: bool = false,
+    super: bool = false,
 };
 
 const DefaultChordTimeoutUs: i64 = 2e6;
@@ -274,85 +231,66 @@ const DownRepeatRate: i64 = 2e4;
 pub const KeyChordPiece = struct {
     // GLFW keys go up to 348, so we use the lower 24 bits for the key
     // and the top 8 bits for the modifiers.
-    value: u32,
+    key: glfw.Key,
+    mod: KeyModifier,
 
-    pub fn from(mod: KeyModifier, k: glfw.Key) KeyChordPiece 
-    {
-        const modValue = @as(u32, @intCast(@intFromEnum(mod))) << 24;
-        const keyValue = @as(u32, @bitCast(@intFromEnum(k))) & 0xffffff;
+    pub fn from(mod: KeyModifier, k: glfw.Key) KeyChordPiece {
         return .{
-            .value = modValue | keyValue,
+            .key = k,
+            .mod = mod,
         };
     }
 
-    pub fn key(self: *const KeyChordPiece) glfw.Key
-    {
-        return @enumFromInt(self.value & 0xffffff);
-    }
-
-    pub fn modifier(self: *const KeyChordPiece) KeyModifier
-    {
-       return @enumFromInt(self.modVal());
-    }
-
-    fn modVal(self: *const KeyChordPiece) u8 {
-        return @as(u8, @intCast((self.value >> 24) & 0xff));
-    }
-
     pub fn print(self: *const KeyChordPiece, buf: []u8) !usize {
-        const mod = self.modVal();
-        const k = self.key();
         var len: usize = 0;
-        if((mod & @intFromEnum(KeyModifier.ctrl)) != 0) {
+        if (self.mod.ctrl) {
             const sl = try std.fmt.bufPrint(buf[len..], "Ctrl+", .{});
             len += sl.len;
         }
-        if((mod & @intFromEnum(KeyModifier.alt)) != 0) {
+        if (self.mod.alt) {
             const sl = try std.fmt.bufPrint(buf[len..], "Alt+", .{});
             len += sl.len;
         }
-        if((mod & @intFromEnum(KeyModifier.shift)) != 0) {
+        if (self.mod.shift) {
             const sl = try std.fmt.bufPrint(buf[len..], "Shift+", .{});
             len += sl.len;
         }
-        if((mod & @intFromEnum(KeyModifier.super)) != 0) {
+        if (self.mod.super) {
             const sl = try std.fmt.bufPrint(buf[len..], "Super+", .{});
             len += sl.len;
         }
 
         const keyNames: [111][]const u8 = .{
-            "space", "'", ",", "-", ".", "/",
-            "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
-            ";", "=", 
-            "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
-            "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X",
-            "Y", "Z", "[", "\\", "]", "`", "world_1", "world_2",
-            "escape", "enter", "tab", "backspace", "insert", "delete",
-            "right", "left", "down", "up",
-            "page_up", "page_down", "home", "end",
-            "caps_lock", "scroll_lock", "num_lock", "print_screen",
-            "pause", 
-            "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9",
-            "F10", "F11", "F12", "F13", "F14", "F15", "F16", "F17",
-            "F18", "F19", "F20", "F21", "F22", "F23", "F24", "F25",
-            "kp_0", "kp_1", "kp_2", "kp_3", "kp_4", "kp_5", "kp_6",
-            "kp_7", "kp_8", "kp_9", "kp_decimal", "kp_divide", 
-            "kp_multiply", "kp_subtract", "kp_add", "kp_enter", "kp_equal",
+            "space",    "'",            ",",          "-",         ".",           "/",
+            "0",        "1",            "2",          "3",         "4",           "5",
+            "6",        "7",            "8",          "9",         ";",           "=",
+            "A",        "B",            "C",          "D",         "E",           "F",
+            "G",        "H",            "I",          "J",         "K",           "L",
+            "M",        "N",            "O",          "P",         "Q",           "R",
+            "S",        "T",            "U",          "V",         "W",           "X",
+            "Y",        "Z",            "[",          "\\",        "]",           "`",
+            "world_1",  "world_2",      "escape",     "enter",     "tab",         "backspace",
+            "insert",   "delete",       "right",      "left",      "down",        "up",
+            "page_up",  "page_down",    "home",       "end",       "caps_lock",   "scroll_lock",
+            "num_lock", "print_screen", "pause",      "F1",        "F2",          "F3",
+            "F4",       "F5",           "F6",         "F7",        "F8",          "F9",
+            "F10",      "F11",          "F12",        "F13",       "F14",         "F15",
+            "F16",      "F17",          "F18",        "F19",       "F20",         "F21",
+            "F22",      "F23",          "F24",        "F25",       "kp_0",        "kp_1",
+            "kp_2",     "kp_3",         "kp_4",       "kp_5",      "kp_6",        "kp_7",
+            "kp_8",     "kp_9",         "kp_decimal", "kp_divide", "kp_multiply", "kp_subtract",
+            "kp_add",   "kp_enter",     "kp_equal",
         };
 
-        const kIdx = getIndexForKey(k) - 1;
-        if(kIdx < keyNames.len) {
+        const kIdx = getIndexForKey(self.key) - 1;
+        if (kIdx < keyNames.len) {
             const sl = try std.fmt.bufPrint(buf[len..], "{s}", .{keyNames[kIdx]});
             len += sl.len;
-        }
-        else {
-            const sl = try std.fmt.bufPrint(buf[len..], "Unknown ({any})", .{k});
+        } else {
+            const sl = try std.fmt.bufPrint(buf[len..], "Unknown ({any})", .{self.key});
             len += sl.len;
         }
-        // inline for(0..keyNames.len) |knIdx| {
-        //     if(kIdx == )
-        // }
-        
+
         return len;
     }
 };
@@ -366,19 +304,19 @@ pub const KeyChord = struct {
 
     pub fn init(alloc: std.mem.Allocator, piece: KeyChordPiece, func: ?[]const u8) !KeyChord {
         var fnc: ?[]const u8 = null;
-        if(fnc != null) {
+        if (fnc != null) {
             fnc = try alloc.dupe(u8, func.?);
         }
         return .{
             .alloc = alloc,
             .func = fnc,
             .piece = piece,
-            .children = std.AutoHashMap(KeyChordPiece, KeyChord).init(alloc)
+            .children = std.AutoHashMap(KeyChordPiece, KeyChord).init(alloc),
         };
     }
 
     pub fn deinit(self: *KeyChord) void {
-        if(self.func != null) {
+        if (self.func != null) {
             self.alloc.free(self.func.?);
         }
 
@@ -387,16 +325,16 @@ pub const KeyChord = struct {
 
     pub fn print(self: *KeyChord, buff: []u8) !usize {
         var len: usize = 0;
-        if(self.func != null) {
+        if (self.func != null) {
             len = try self.piece.print(buff);
             const sl = try std.fmt.bufPrint(buff[len..], ": {s}\n", .{self.func.?});
             len += sl.len;
         }
 
         var it = self.children.keyIterator();
-        while(it.next()) |k| {
+        while (it.next()) |k| {
             len += try self.piece.print(buff[len..]);
-            _= try std.fmt.bufPrint(buff[len..], ", ", .{});
+            _ = try std.fmt.bufPrint(buff[len..], ", ", .{});
             len += 2;
 
             len += try k.print(buff[len..]);
@@ -415,10 +353,10 @@ const ChordTree = struct {
     rootChord: KeyChord,
     elapsedUsCounter: i64,
     repeatCounter: i64,
-    
+
     pub fn init(alloc: std.mem.Allocator, context: ?[]const u8) !ChordTree {
         var ctxt: ?[]const u8 = null;
-        if(ctxt != null) {
+        if (ctxt != null) {
             ctxt = try alloc.dupe(u8, context.?);
         }
 
@@ -427,14 +365,14 @@ const ChordTree = struct {
             .context = ctxt,
             .downKey = .unknown,
             .currChord = null,
-            .rootChord = try KeyChord.init(alloc, KeyChordPiece.from(.none, .unknown), null),
+            .rootChord = try KeyChord.init(alloc, KeyChordPiece.from(.{}, .unknown), null),
             .elapsedUsCounter = 0,
             .repeatCounter = 0,
         };
     }
 
     pub fn deinit(self: *ChordTree) void {
-        if(self.context != null) {
+        if (self.context != null) {
             self.alloc.free(self.context);
         }
 
@@ -464,35 +402,34 @@ pub const KeyMap = struct {
     }
 
     pub fn deinit(self: *KeyMap) void {
-       _ = self;
-
+        _ = self;
     }
 
     pub fn addKeyChord(self: *KeyMap, mods: KeyModifier, key: glfw.Key, func: []const u8, context: ?[]const u8) !bool {
-       _ = self;
-       _ = context;
-       _ = func;
-       _ = key;
-       _ = mods;
+        _ = self;
+        _ = context;
+        _ = func;
+        _ = key;
+        _ = mods;
         return false;
     }
 
     pub fn addTwoKeyChord(self: *KeyMap, mods: KeyModifier, key1: glfw.Key, key2: glfw.Key, func: []const u8, context: ?[]const u8) !bool {
-       _ = self;
-       _ = key2;
-       _ = context;
-       _ = func;
-       _ = key1;
-       _ = mods;
+        _ = self;
+        _ = key2;
+        _ = context;
+        _ = func;
+        _ = key1;
+        _ = mods;
         return false;
     }
 
     pub fn addComplexChord(self: *KeyMap, pkp1: KeyChordPiece, kp2: KeyChordPiece, func: []const u8, context: ?[]const u8) !bool {
-       _ = pkp1;
-       _ = self;
-       _ = context;
-       _ = func;
-       _ = kp2;
+        _ = pkp1;
+        _ = self;
+        _ = context;
+        _ = func;
+        _ = kp2;
         return false;
     }
 
--- tests/keymap_tests.zig
+++ tests/keymap_tests.zig
@@ -7,18 +7,18 @@ pub fn simpleChordTest() !void {
     var km = try KeyMap.init(std.heap.page_allocator);
     defer km.deinit();
 
-    _ = try km.addKeyChord(.ctrl, .a, "test", null);
+    _ = try km.addKeyChord(.{ .ctrl = true }, .a, "test", null);
 }
 
 pub fn printKeyChordPieceTest() !void {
-    const kp1 = input.KeyChordPiece.from(.ctrl_shift, .a);
+    const kp1 = input.KeyChordPiece.from(.{ .ctrl = true, .shift = true }, .a);
 
     var buff: [256]u8 = undefined;
     const len1 = try kp1.print(buff[0..]);
     try testz.expectTrue(std.mem.eql(u8, buff[0..len1], "Ctrl+Shift+A"));
 
     var kc = try input.KeyChord.init(std.heap.page_allocator, kp1, "test");
-    const kp2 = input.KeyChordPiece.from(.alt, .p);
+    const kp2 = input.KeyChordPiece.from(.{ .alt = true }, .p);
     const kc2 = try input.KeyChord.init(std.heap.page_allocator, kp2, "another");
     try kc.children.put(kp2, kc2);
     const len2 = try kc.print(buff[0..]);
