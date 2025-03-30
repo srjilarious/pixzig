96cba6de65ecc433f60ecdd5cc592829b54c706d
Jeff DeWall <sr.jilarious@fastmail.net>
2025-03-30 09:34:51 -0600

    Moved digcraft to own repo.

--- games/digcraft/constants.zig
+++
@@ -1,7 +0,0 @@
-pub const Scale = 3;
-pub const NumTilesHorz = 16;
-pub const NumTilesVert = 16;
-pub const TileWidth = 16;
-pub const TileHeight = 16;
-pub const MapWidth = 50;
-pub const MapHeight = 30;
